022cc338fab9fc76e17ee4226e9da8633036800b
Adam Eijdenberg <adam.eijdenberg@digital.gov.au>
2018-05-23 17:24:37 +1000

    Add report-buildpacks

--- cmd/report-users/report-users.go
+++ cmd/report-users/report-users.go
@@ -5,8 +5,10 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"math"
 	"net/http"
 	"os"
+	"time"
 
 	"code.cloudfoundry.org/cli/plugin"
 	"github.com/olekukonko/tablewriter"
@@ -70,91 +72,167 @@ func (sc *simpleClient) List(r string, f func(*resource) error) error {
 // resource captures fields that we care about when
 // retrieving data from CloudFoundry
 type resource struct {
+	Metadata struct {
+		UpdatedAt time.Time `json:"updated_at"` // buildpack
+	} `json:"metadata"`
 	Entity struct {
-		Name               string // org, space
-		SpacesURL          string `json:"spaces_url"`           // org
-		UsersURL           string `json:"users_url"`            // org
-		ManagersURL        string `json:"managers_url"`         // org, space
-		BillingManagersURL string `json:"billing_managers_url"` // org
-		AuditorsURL        string `json:"auditors_url"`         // org, space
-		DevelopersURL      string `json:"developers_url"`       // space
-		Admin              bool   // user
-		Username           string // user
-	}
+		Name               string    // org, space
+		SpacesURL          string    `json:"spaces_url"`              // org
+		UsersURL           string    `json:"users_url"`               // org
+		ManagersURL        string    `json:"managers_url"`            // org, space
+		BillingManagersURL string    `json:"billing_managers_url"`    // org
+		AuditorsURL        string    `json:"auditors_url"`            // org, space
+		DevelopersURL      string    `json:"developers_url"`          // space
+		AppsURL            string    `json:"apps_url"`                // space
+		BuildpackGUID      string    `json:"detected_buildpack_guid"` // app
+		Buildpack          string    `json:"buildpack"`               // app
+		Admin              bool      // user
+		Username           string    // user
+		Filename           string    `json:"filename"`           // buildpack
+		Enabled            bool      `json:"enabled"`            // buildpack
+		PackageUpdatedAt   time.Time `json:"package_updated_at"` // app
+	} `json:"entity"`
 }
 
 type reportUsers struct{}
 
+func newSimpleClient(cliConnection plugin.CliConnection) (*simpleClient, error) {
+	at, err := cliConnection.AccessToken()
+	if err != nil {
+		return nil, err
+	}
+
+	api, err := cliConnection.ApiEndpoint()
+	if err != nil {
+		return nil, err
+	}
+
+	return &simpleClient{
+		API:           api,
+		Authorization: at,
+	}, nil
+}
+
 func (c *reportUsers) Run(cliConnection plugin.CliConnection, args []string) {
-	if args[0] == "report-users" {
+	switch args[0] {
+	case "report-users":
 		if len(args) != 1 {
-			fmt.Println("Expected no args")
-			os.Exit(1)
+			log.Fatal("Expected no args")
 		}
-
-		at, err := cliConnection.AccessToken()
+		err := c.reportUsers(cliConnection)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		api, err := cliConnection.ApiEndpoint()
+	case "report-buildpacks":
+		if len(args) != 1 {
+			log.Fatal("Expected no args")
+		}
+		err := c.reportBuildpacks(cliConnection)
 		if err != nil {
 			log.Fatal(err)
 		}
+	}
+}
 
-		client := &simpleClient{
-			API:           api,
-			Authorization: at,
+func (c *reportUsers) reportBuildpacks(cliConnection plugin.CliConnection) error {
+	client, err := newSimpleClient(cliConnection)
+	if err != nil {
+		return err
+	}
+
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Organization", "Space", "Application", "Buildpack", "Out of date"})
+
+	buildpacks := make(map[string]*resource)
+
+	err = client.List("/v2/organizations", func(org *resource) error {
+		return client.List(org.Entity.SpacesURL, func(space *resource) error {
+			return client.List(space.Entity.AppsURL, func(app *resource) error {
+				bp, ok := buildpacks[app.Entity.BuildpackGUID]
+				if !ok {
+					var bbp resource
+					err := client.Get(fmt.Sprintf("/v2/buildpacks/%s", app.Entity.BuildpackGUID), &bbp)
+					if err != nil {
+						bbp.Entity.Filename = app.Entity.Buildpack
+						bbp.Entity.Enabled = false
+					}
+					buildpacks[app.Entity.BuildpackGUID] = &bbp
+					bp = &bbp
+				}
+				ood := ""
+				if bp.Metadata.UpdatedAt.After(app.Entity.PackageUpdatedAt) {
+					ood = fmt.Sprintf("%d days", int(math.Ceil(bp.Metadata.UpdatedAt.Sub(app.Entity.PackageUpdatedAt).Hours()/24.0)))
+				} else if !bp.Entity.Enabled {
+					ood = "Needs attention"
+				}
+				table.Append([]string{org.Entity.Name, space.Entity.Name, app.Entity.Name, app.Entity.Buildpack, ood})
+				return nil
+			})
+		})
+	})
+	if err != nil {
+		return err
+	}
+
+	table.Render()
+	return nil
+}
+
+func (c *reportUsers) reportUsers(cliConnection plugin.CliConnection) error {
+	client, err := newSimpleClient(cliConnection)
+	if err != nil {
+		return err
+	}
+
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Organization", "Space", "Username", "Role"})
+
+	err = client.List("/v2/organizations", func(org *resource) error {
+		for _, orgRole := range []struct {
+			Role string
+			URL  string
+		}{
+			//{"OrgUser", org.Entity.UsersURL}, // These don't appear to be meaningful
+			{"OrgManager", org.Entity.ManagersURL},
+			{"OrgBillingManager", org.Entity.BillingManagersURL},
+			{"OrgAuditor", org.Entity.AuditorsURL},
+		} {
+			err = client.List(orgRole.URL, func(user *resource) error {
+				table.Append([]string{org.Entity.Name, "n/a", user.Entity.Username, orgRole.Role})
+				return nil
+			})
+			if err != nil {
+				return err
+			}
 		}
 
-		table := tablewriter.NewWriter(os.Stdout)
-		table.SetHeader([]string{"Organization", "Space", "Username", "Role"})
-
-		err = client.List("/v2/organizations", func(org *resource) error {
-			for _, orgRole := range []struct {
+		return client.List(org.Entity.SpacesURL, func(space *resource) error {
+			for _, spaceRole := range []struct {
 				Role string
 				URL  string
 			}{
-				//{"OrgUser", org.Entity.UsersURL}, // These don't appear to be meaningful
-				{"OrgManager", org.Entity.ManagersURL},
-				{"OrgBillingManager", org.Entity.BillingManagersURL},
-				{"OrgAuditor", org.Entity.AuditorsURL},
+				{"SpaceDeveloper", space.Entity.DevelopersURL},
+				{"SpaceManager", space.Entity.ManagersURL},
+				{"SpaceAuditor", space.Entity.AuditorsURL},
 			} {
-				err = client.List(orgRole.URL, func(user *resource) error {
-					table.Append([]string{org.Entity.Name, "n/a", user.Entity.Username, orgRole.Role})
+				err = client.List(spaceRole.URL, func(user *resource) error {
+					table.Append([]string{org.Entity.Name, space.Entity.Name, user.Entity.Username, spaceRole.Role})
 					return nil
 				})
 				if err != nil {
 					return err
 				}
 			}
-
-			return client.List(org.Entity.SpacesURL, func(space *resource) error {
-				for _, spaceRole := range []struct {
-					Role string
-					URL  string
-				}{
-					{"SpaceDeveloper", space.Entity.DevelopersURL},
-					{"SpaceManager", space.Entity.ManagersURL},
-					{"SpaceAuditor", space.Entity.AuditorsURL},
-				} {
-					err = client.List(spaceRole.URL, func(user *resource) error {
-						table.Append([]string{org.Entity.Name, space.Entity.Name, user.Entity.Username, spaceRole.Role})
-						return nil
-					})
-					if err != nil {
-						return err
-					}
-				}
-				return nil
-			})
+			return nil
 		})
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		table.Render()
+	})
+	if err != nil {
+		return err
 	}
+
+	table.Render()
+	return nil
 }
 
 func (c *reportUsers) GetMetadata() plugin.PluginMetadata {
@@ -162,7 +240,7 @@ func (c *reportUsers) GetMetadata() plugin.PluginMetadata {
 		Name: "Report Users",
 		Version: plugin.VersionType{
 			Major: 0,
-			Minor: 2,
+			Minor: 3,
 			Build: 0,
 		},
 		MinCliVersion: plugin.VersionType{
@@ -176,6 +254,13 @@ func (c *reportUsers) GetMetadata() plugin.PluginMetadata {
 				HelpText: "Report all users in installation",
 				UsageDetails: plugin.Usage{
 					Usage: "report-users\n   cf report-users",
+				},
+			},
+			{
+				Name:     "report-buildpacks",
+				HelpText: "Report all buildpacks used in installation",
+				UsageDetails: plugin.Usage{
+					Usage: "report-buildpacks\n   cf report-buildpacks",
 				},
 			},
 		},
